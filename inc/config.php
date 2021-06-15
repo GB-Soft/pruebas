e62f533c0df3e4ada1db78a075a7998d90c7680f
GB-Soft <85730067+GB-Soft@users.noreply.github.com>
2021-06-15 08:07:31 +0200

    Delete inc directory

--- inc/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("_WWW_", "https://interfaz.e-commercego.com/");
-define("_EMAIL_ADMIN_",'soporte@goproducts.es');
-define("_APP_NAME_",'E-Go Tools');
-
-/*if (session_status() == PHP_SESSION_NONE) {
-	session_start();
-}*/
